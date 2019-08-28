4ce8ddd1ab1db68d13cb54420ebf10a42f9d089f
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2019-08-28 16:00:05 +0100

    set up for Heroku server. Burger colour changes with breakpoints. A few changes to the problems area section text.

--- app.js
+++ app.js
@@ -58,8 +58,7 @@ app.post("/", function(req, res){
 
 
 
-
 // Server listening
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server now listening on port 3000");
 });
